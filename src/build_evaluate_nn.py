7dd467753a41f46df2640971b1fb5da8cba03e82
KatarzynaLauresh <katarzyna@lauresh.net>
2019-06-18 11:01:11 +0200

    added .gitignore, improved build_evaluate_nn module

--- src/build_evaluate_nn.py
+++ src/build_evaluate_nn.py
@@ -4,7 +4,7 @@ from keras.layers import Embedding
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import h5py
 
-def build_nn(list_of_layers):
+def build_nn(emb_matrix, max_len, list_of_layers):
   '''Build neural network from a given list of layers.'''
   model = Sequential()
   model.add(Embedding(emb_matrix.shape[0],
@@ -23,18 +23,18 @@ def build_nn(list_of_layers):
 
 
 
-def evaluate_nn(model, X_train, X_val, y_train, y_val, filename, batch_size=32, early_stopping=False):
+def evaluate_nn(model, X_train, X_val, y_train, y_val, filename, batch_size=32, early_stopping=False, epochs=50):
   '''Fit and evaluate neural network on validation data.'''
   take_best_model = ModelCheckpoint(str(filename)+".h5py", save_best_only=True)
   
   if early_stopping == True:
     early_stopping = EarlyStopping(patience=10, monitor="val_loss")
-    history = model.fit(X_train, y_train, epochs=50, validation_split=0.2,
+    history = model.fit(X_train, y_train, epochs=epochs, validation_split=0.2,
               batch_size=batch_size, 
               callbacks=[early_stopping, take_best_model])
    
   else:
-    history = model.fit(X_train, y_train, epochs=50, validation_split=0.2,
+    history = model.fit(X_train, y_train, epochs=epochs, validation_split=0.2,
               batch_size=batch_size, 
               callbacks=[take_best_model])
   
